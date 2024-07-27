8735ae9d7ab61f581b0cb566d3f580f8e525aee6
Pratiksha Shelke <shelkepratiksha198@gmail.com>
2024-07-27 12:36:44 +0530

    Add files via upload

---
+++ 01.php
@@ -0,0 +1,35 @@
+<?php
+//echo "pratiksha";
+//var_dump("hello");
+
+ //$mobile_number=(int) "1234567890";
+//var_dump($mobile_number);
+ //echo $mobile_number;
+ //var_dump(intval("hello"));
+ //$name="pratiksha";
+ //echo (strval($name));
+ //var_dump (strval("pratiksha"));
+ $name="pratiksha";
+ echo "$name";
+ echo '<br>';
+ echo ucfirst($name);
+echo '<br>';
+echo ucwords($name);
+echo '<br>'; 
+echo strtoupper($name);
+echo '<br>';
+echo strtolower ($name);
+echo '<br>';
+
+var_dump (10/2);
+
+
+$n1=10;
+$n2=10;
+var_dump($n1 <=> $n2);
+
+
+
+ 
+ 
+?>
